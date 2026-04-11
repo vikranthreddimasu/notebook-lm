62db63f5bb19e606d38bee45b2ee849db0a1532d
Vikranth Reddimasu <vikranthreddimasu@gmail.com>
2026-04-11 13:52:36 -0400

    feat: expose backendUrl and onBackendReady in preload bridge

--- apps/desktop/electron/preload.cjs
+++ apps/desktop/electron/preload.cjs
@@ -1,8 +1,12 @@
+// apps/desktop/electron/preload.cjs
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('notebookBridge', {
   ping: () => ipcRenderer.invoke('app:ping'),
   choosePath: (options) => ipcRenderer.invoke('dialog:choosePath', options),
   openExternal: (url) => ipcRenderer.invoke('app:openExternal', url),
+  backendUrl: () => ipcRenderer.invoke('app:backendUrl'),
+  onBackendReady: (callback) => {
+    ipcRenderer.on('app:backendReady', (_, url) => callback(url));
+  },
 });
-
